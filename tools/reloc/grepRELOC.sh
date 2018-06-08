47fd8f716bb72c1e5fb540e2d1ba6196700efc4f
matteo <matteo@bagagli.ethz.ch>
2018-06-08 12:42:51 +0200

    stilla djusting. Need to fix grep_reloc script

--- tools/reloc/grepRELOC.sh
+++ tools/reloc/grepRELOC.sh
@@ -7,20 +7,32 @@
 # AUTHOR: Matteo Bagagli @ INGV.PI
 # DATE:   03/2014
 
-if [ "$#" -ne "2" ] then;
-    echo "USAGE: (basename $0) DIRPATH SAVEPATH"
+# These lines need to be added in run_pstomo script (in order to save the Event_err log file
+#  if (-e Event_err.log)  then
+#     \mv Event_err.log Event_err_$i.log
+#  endif
+
+
+
+if [ "$#" -ne "2" ]; then
+    echo "USAGE: $(basename $0) DIRPATH SAVEPATH"
     exit
 fi
 # ================================================
 dir=${1}
 out_dir=${2}
 
-cd ./${dir}
+cd ${dir}
 
-for i in `ls | grep Event_err | cat -n | awk {'print $1'}`; do
-    grep km Event_err_${i}.log | awk '{print $2, $4, $6, $9}' > Rel_${dir}.${i}
-    cp Rel_${dir}.${i} $out_dir
-    echo "...  ${i} file stored in ${out_dir} !!!"
+for ii in `ls | grep Event_err | cat -n | awk {'print $1'}`; do
+ echo "${ii}"
+done
+
+
+for ii in `ls | grep Event_err | cat -n | awk {'print $1'}`; do
+    grep km Event_err_${ii}.log | awk '{print $2, $4, $6, $9}' > Rel_${dir}.${ii}
+    cp Rel_${dir}.${ii} $out_dir
+    echo "...  ${ii} file stored in ${out_dir} !!!"
 done
 echo "... DONE"
 
